ee4ff772e92f9b32de24068110d0e26766e5be17
femsy <218602992+f3msy@users.noreply.github.com>
2026-06-24 19:01:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const config = require('./config.json');
 /** @type {Set<import('discord.js-selfbot-v13').Client>} */
 const clients = new Set();
 
+const blue = '\x1b[34m';
+const bold = '\x1b[1m';
+const reset = '\x1b[0m';
+
 for (const token of config.tokens) {
     for (const [browser, device] of Object.entries(config.devices)) {
         if (!device.spoof) continue;
@@ -13,12 +17,14 @@ for (const token of config.tokens) {
         /** @type {import('discord.js-selfbot-v13').Client} */
         const client = new Client({ ws: { properties: { browser } } });
 
+        console.clear();
+
         client.once('ready', () => {
             if (config.status === "online") client.user?.setStatus("online");
             else if (config.status === "idle") client.user?.setStatus("idle");
             else if (config.status === "dnd") client.user?.setStatus("dnd");
-            
-            console.log(`spoofed as ${browser} on ${device.name} | ${client.user?.username}`);
+    
+            console.log(`${bold}spoofed as ${blue}${browser}${reset} on ${blue}${bold}${device.name}${reset} | ${bold}${client.user?.username}${reset}`);
         });
 
         client.login(token).catch(console.error);
